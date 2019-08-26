5201b08ecb4d954a65063cb4590ea8cfb7c635ad
mxm <1130198893@qq.com>
2019-08-26 13:43:53 +0800

    😴

--- core/src/main/java/com/daveboy/core/mvvm/BaseViewModel.kt
+++ core/src/main/java/com/daveboy/core/mvvm/BaseViewModel.kt
@@ -2,8 +2,24 @@ package com.daveboy.core.mvvm
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.daveboy.core.http.core.IResponse
+import com.daveboy.core.util.paresException
+import com.daveboy.core.util.parseResult
+import kotlinx.coroutines.launch
 
 abstract class BaseViewModel: ViewModel() {
-
+    fun <T>launchRequest(request:suspend ()->IResponse<T>,viewStateLiveData:MutableLiveData<ViewState<T>>){
+        viewModelScope.launch {
+            runCatching {
+                viewStateLiveData.value= ViewState.onLoading()
+                request()
+            }.onSuccess {
+                viewStateLiveData.parseResult(it)
+            }.onFailure {
+                viewStateLiveData.paresException(it)
+            }
+        }
+    }
 
 }
--- core/src/main/java/com/daveboy/core/util/ViewStateUtil.kt
+++ core/src/main/java/com/daveboy/core/util/ViewStateUtil.kt
@@ -1,13 +1,17 @@
 package com.daveboy.core.util
 
 import androidx.lifecycle.MutableLiveData
-import com.daveboy.core.http.base.BaseResponse
+import com.daveboy.core.http.core.IResponse
 import com.daveboy.core.mvvm.CustomException
 import com.daveboy.core.mvvm.ViewState
 
-fun <T>MutableLiveData<ViewState<T>>.parseResult(response:BaseResponse<T>){
-    when{
-        response.isSuccess()->this.value= ViewState.onSuccess(response.getData())
-        else ->this.value= ViewState.onError(CustomException( response.getMessage()))
+fun <T> MutableLiveData<ViewState<T>>.parseResult(response: IResponse<T>) {
+    when {
+        response.isSuccess() -> this.value = ViewState.onSuccess(response.getData())
+        else -> this.value = ViewState.onError(CustomException(response.getMessage()))
     }
+}
+
+fun <T> MutableLiveData<ViewState<T>>.paresException(exception: Throwable) {
+    this.value = ViewState.onError(CustomException(exception))
 }
